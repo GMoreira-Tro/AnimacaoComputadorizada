1001d83765ddbe78e5436f9e6dac0e5dcaf8d55d
xRodrigoSx <rodrgosantos123@hotmail.com>
2024-11-26 14:50:00 -0300

    fix eyes and cam position

--- Modelagem-lutador/Assets/FighterController.cs
+++ Modelagem-lutador/Assets/FighterController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class FighterController : MonoBehaviour
 {
     public float raySpacing = 1f;
+    public Transform eyesPosition;
     public interface FighterState
     {
 
@@ -18,51 +19,49 @@ public class FighterController : MonoBehaviour
 
             fighter.transform.RotateAround(fighter.transform.position, Vector3.up, 20 * Time.deltaTime);
 
-           
-            for (int i = 0; i < 5; i++) {
 
+            for (int i = 0; i < 5; i++)
+            {
                 RaycastHit hit;
 
-                Vector3 direction = fighter.transform.forward + new Vector3(i * fighter.raySpacing, 0f, 0f);
-                Ray ray = new Ray(fighter.transform.position, direction.normalized);
+                Vector3 direction = (fighter.transform.forward + fighter.transform.right * (i - 2) * fighter.raySpacing).normalized;
+
+                // Usar a posição dos olhos como origem do raio
+                Vector3 rayOrigin = fighter.eyesPosition.position;
+
+                Ray ray = new Ray(rayOrigin, direction);
+
                 if (Physics.Raycast(ray, out hit, 1000f))
                 {
-                    Debug.DrawRay(fighter.transform.position, direction * hit.distance, Color.yellow);
-
+                    Debug.DrawRay(rayOrigin, direction * hit.distance, Color.yellow);
                     Debug.Log("bubble");
-
                 }
                 else
                 {
-                    Debug.DrawRay(fighter.transform.position, direction, Color.yellow);
+                    Debug.DrawRay(rayOrigin, direction * 10f, Color.red);
                     Debug.Log("no hit");
                 }
             }
-          
+
             if (Input.GetKeyDown(KeyCode.J))
             {
                 fighter._state = new GuardState();
-                //fighter.animator.SetTrigger("Guard");
             }
             if (Input.GetKeyDown(KeyCode.K))
             {
                 fighter._state = new MahTawaState();
-                //fighter.animator.SetTrigger("MahTawa");
             }
             if (Input.GetKeyDown(KeyCode.L))
             {
                 fighter._state = new JasunbalTolioState();
-                //fighter.animator.SetTrigger("JasunbalTolio");
             }
             if (Input.GetKeyDown(KeyCode.N))
             {
                 fighter._state = new MondolioRurioState();
-                //fighter.animator.SetTrigger("MondolioRurio");
             }
             if (Input.GetKeyDown(KeyCode.M))
             {
                 fighter._state = new DubonTolioState();
-                //fighter.animator.SetTrigger("DubonTolio");
             }
         }
         public void update(FighterController fighter)
@@ -147,28 +146,6 @@ public class FighterController : MonoBehaviour
 
     void Update()
     {
-        /*
-        if (Input.GetKeyDown(KeyCode.J))
-        {
-            animator.SetTrigger("Guard");
-        }
-        if (Input.GetKeyDown(KeyCode.K))
-        {
-            animator.SetTrigger("MahTawa");
-        }
-        if (Input.GetKeyDown(KeyCode.L))
-        {
-            animator.SetTrigger("JasunbalTolio");
-        }
-        if (Input.GetKeyDown(KeyCode.N))
-        {
-            animator.SetTrigger("MondolioRurio");
-        }
-        if (Input.GetKeyDown(KeyCode.M))
-        {
-            animator.SetTrigger("DubonTolio");
-        }*/
-
         _state.handleInput(this);
         _state.update(this);
 
